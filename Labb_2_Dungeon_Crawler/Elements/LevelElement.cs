da09df3ed1dbc115d1899cab1a0e7caaacd147a8
SofiaNords <sofia.k.nordstrom.com>
2024-09-26 16:16:48 +0200

    Add position- and char props to LevelElement

--- Labb_2_Dungeon_Crawler/Elements/LevelElement.cs
+++ Labb_2_Dungeon_Crawler/Elements/LevelElement.cs
@@ -1,4 +1,9 @@
 ﻿public abstract class LevelElement
 {
+    public abstract int PositionX { get; set; }
+    public abstract int PositionY { get; set; }
+
+    public char ClassChar { get; set; }
+    public ConsoleColor Color { get; set; }
 
 }
