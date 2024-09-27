8cdfd2be3eb455c1bfbc5691736b609cc5f4c6f5
SofiaNords <sofia.k.nordstrom.com>
2024-09-27 12:47:31 +0200

    Create Player constructor in Player class

--- Labb_2_Dungeon_Crawler/Player.cs
+++ Labb_2_Dungeon_Crawler/Player.cs
@@ -1,4 +1,10 @@
-﻿class Player
+﻿public class Player : LevelElement
 {
-
+    public Player(int x, int y)
+    {
+        ClassChar = '@';
+        Color = ConsoleColor.Magenta;
+        PositionX = x;
+        PositionY = y;
+    }
 }
