69eeaa42e08957bbb7bad1c40bdce636e6ae4958
SofiaNords <sofia.k.nordstrom.com>
2024-09-28 09:11:26 +0200

    Add comments to LevelData

--- Labb_2_Dungeon_Crawler/LevelData.cs
+++ Labb_2_Dungeon_Crawler/LevelData.cs
@@ -1,74 +1,84 @@
-﻿
-
-public class LevelData
+﻿public class LevelData
 {
+    // List to hold the elements that make up the level
     private List<LevelElement> _elements;
 
+    // Exposing the elements list publicly, allowing read access while keeping the list itself private
     public List<LevelElement> Elements
     {
         get { return _elements; }
     }
 
+    // Constructor to initialize the level elements list
     public LevelData()
     {
         _elements = new List<LevelElement>();
     }
 
+    // Method to load level data from a specified file
     public void Load(string fileName)
     {
+        // Constructing the file path to locate the level file
         string filePath = Path.Combine("Levels", fileName);
 
         try
         {
+            // Using StreamReader to read the level file content
             using (StreamReader reader = new StreamReader(filePath))
             {
                 int character;
-                int positionX = 0;
-                int positionY = 0;
+                int positionX = 0; // Tracks the horizontal position in the level
+                int positionY = 0; // Tracks the vertical position in the level
 
+                // Reading characters from the file until the end is reached
                 while ((character = reader.Read()) != -1)
                 {
-                    char c = (char)character;
+                    char c = (char)character; // Casting the character to char type
 
-                    LevelElement element = null;
+                    LevelElement element = null; // Variable to hold the level element being created
 
+                    // Determining which type of level element to create based on the character read
                     switch (c)
                     {
                         case '#':
-                            element = new Wall(positionX, positionY);
+                            element = new Wall(positionX, positionY); // Create a wall element
                             break;
                         case 'r':
-                            element = new Rat(positionX, positionY);
+                            element = new Rat(positionX, positionY); // Create a rat element
                             break;
                         case 's':
-                            element = new Snake(positionX, positionY);
+                            element = new Snake(positionX, positionY); // Create a snake element
                             break;
                         case '@':
-                            element = new Player(positionX, positionY);
+                            element = new Player(positionX, positionY); // Create a player element
                             break;
                         default:
-                            element = null;
+                            element = null; // Ignore unrecognized characters
                             break;
                     };
 
+                    // Adding the created element to the list if it is valid
                     if (element != null)
                     {
-                        _elements.Add(element); 
+                        _elements.Add(element);
                     }
 
+                    // Incrementing the x position for the next character
                     positionX++;
 
+                    // If a new line character is encountered, reset x position and increment y position
                     if (c == '\n')
                     {
                         positionY++;
-                        positionX = 0;
+                        positionX = 0; // Resetting to the start of the next line
                     }
                 }
             }
         }
         catch (IOException e)
         {
-            Console.WriteLine("Ett fel uppstod");
+            // Handling potential file I/O errors
+            Console.WriteLine("An error occurred");
             Console.WriteLine(e.Message);
         }
     }
