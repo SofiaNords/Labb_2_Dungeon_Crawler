6776e06853fa64d726c6dbffdd94355a51b7b0cf
SofiaNords <sofia.k.nordstrom.com>
2024-09-28 09:38:44 +0200

    Create Update method in Enemy class

--- Labb_2_Dungeon_Crawler/Elements/Enemy.cs
+++ Labb_2_Dungeon_Crawler/Elements/Enemy.cs
@@ -1,6 +1,11 @@
-﻿public abstract class Enemy : LevelElement
+﻿using System.Data;
+
+public abstract class Enemy : LevelElement
 {
     public virtual string Name { get; set; }
 
     public virtual int HP { get; set; }
+
+    public abstract void Update();
+  
 }
