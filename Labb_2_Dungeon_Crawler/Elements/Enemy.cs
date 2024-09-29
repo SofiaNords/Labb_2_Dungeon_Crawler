ad2e8747a7f6967419bff324e90116dabb5d8097
SofiaNords <sofia.k.nordstrom.com>
2024-09-29 13:59:53 +0200

    Change datatype to Attack and Defence props

--- Labb_2_Dungeon_Crawler/Elements/Enemy.cs
+++ Labb_2_Dungeon_Crawler/Elements/Enemy.cs
@@ -6,9 +6,9 @@ public abstract class Enemy : LevelElement
 
     public virtual int HP { get; set; }
 
-    public int AttackDice { get; set; }
+    public Dice AttackDice { get; set; }
 
-    public int DefenceDice { get; set; }
+    public Dice DefenceDice { get; set; }
 
     public abstract void Update();
   
