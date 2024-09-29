bc31504877b411e604161941cb0027748e4944d4
SofiaNords <sofia.k.nordstrom.com>
2024-09-29 11:26:58 +0200

    Create Dice constructor in Dice class

--- Labb_2_Dungeon_Crawler/Dice.cs
+++ Labb_2_Dungeon_Crawler/Dice.cs
@@ -1,4 +1,15 @@
 ﻿public class Dice
 {
+    private int _numberOfDice;
+    private int _sidesPerDice;
+    private int _modifier;
+
+    public Dice(int numberOfDice, int sidesPerDice, int modifier)
+    {
+        this._numberOfDice = numberOfDice;
+        this._sidesPerDice = sidesPerDice;
+        this._modifier = modifier;
+    }
+
 
 }
