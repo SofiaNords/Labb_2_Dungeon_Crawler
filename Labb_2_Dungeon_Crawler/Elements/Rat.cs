738edc6a70048a7e8f1f1488c8def10645082391
SofiaNords <sofia.k.nordstrom.com>
2024-09-29 15:23:26 +0200

    Add movement patterns in Update() in Rat class

--- Labb_2_Dungeon_Crawler/Elements/Rat.cs
+++ Labb_2_Dungeon_Crawler/Elements/Rat.cs
@@ -1,4 +1,6 @@
-﻿public class Rat : Enemy
+﻿using System.Numerics;
+
+public class Rat : Enemy
 {
     public Rat(int x, int y)
     {
@@ -10,8 +12,56 @@
         HP = 10;
     }
 
+    private LevelData _levelData;
+
     public override void Update()
     {
-        throw new NotImplementedException();
+        int direction;
+        int newX = this.PositionX;
+        int newY = this.PositionY;
+
+        int up = 1;
+        int down = 2;
+        int left = 3;
+        int right = 4;
+
+        Random random = new Random();
+
+        direction = random.Next(1, 4);
+
+        if (direction == up)
+        {
+            newY--;
+        } 
+        else if (direction == down)
+        {
+            newY++;
+        }
+        else if (direction == left)
+        {
+            newX--;
+        }
+        else if (direction == right)
+        {
+            newX++;
+        }
+
+        if (IsValidMove(newX, newY))
+        {
+            this.PositionX = newX;
+            this.PositionY = newY;
+        }
+    }
+
+    private bool IsValidMove(int x, int y)
+    {
+        foreach (var element in _levelData.Elements)
+        {
+            if (element.PositionX == x && element.PositionY == y && (element is Wall || element is Enemy || element is Player))
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
