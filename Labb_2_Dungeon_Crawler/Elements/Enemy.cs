7e2d5de6ffa97e0ea7dc57466663b34c98d91274
SofiaNords <sofia.k.nordstrom.com>
2024-09-29 13:58:16 +0200

    Add Attack and Defence props to Enemy class

--- Labb_2_Dungeon_Crawler/Elements/Enemy.cs
+++ Labb_2_Dungeon_Crawler/Elements/Enemy.cs
@@ -6,6 +6,10 @@ public abstract class Enemy : LevelElement
 
     public virtual int HP { get; set; }
 
+    public int AttackDice { get; set; }
+
+    public int DefenceDice { get; set; }
+
     public abstract void Update();
   
 }
