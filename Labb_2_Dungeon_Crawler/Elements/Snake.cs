49d7761032d23f7d5a636e7ef822b222d6c4eaab
SofiaNords <sofia.k.nordstrom.com>
2024-09-30 16:50:26 +0200

    Update Update() & IsValidMove in Snake class

--- Labb_2_Dungeon_Crawler/Elements/Snake.cs
+++ Labb_2_Dungeon_Crawler/Elements/Snake.cs
@@ -1,7 +1,9 @@
 ﻿public class Snake : Enemy
 {
+    private LevelData _levelData;
     private Player _player;
-    public Snake(int x, int y, Player player)
+
+    public Snake(int x, int y, LevelData levelData, Player player)
     {
         ClassChar = 's';
         Color = ConsoleColor.Green;
@@ -9,11 +11,58 @@
         PositionY = y;
         Name = "Snake";
         HP = 25;
+        _levelData = levelData;
         _player = player;
     }
 
     public override void Update()
     {
-       
+        int playerPositionX = _player.PositionX;
+        int playerPositionY = _player.PositionY;
+
+        int distanceX = Math.Abs(PositionX - playerPositionX);
+        int distanceY = Math.Abs(PositionY - playerPositionY);
+
+        int newX = PositionX;
+        int newY = PositionY;
+
+        if (distanceX <= 2 && distanceY <= 2)
+        {
+            if (PositionX < playerPositionX)
+            {
+                newX--;
+            }
+            else
+            {
+                newX++;
+            }
+
+            if (PositionY < playerPositionY)
+            {
+                newY--;
+            }
+            else
+            {
+                newY++;
+            }
+        }
+
+        if (IsValidMove(newX, newY))
+        {
+            PositionX = newX;
+            PositionY = newY;
+        }
+    }
+
+    private bool IsValidMove(int x, int y)
+    {
+        foreach (var element in _levelData.Elements)
+        {
+            if (element.PositionX == x && element.PositionY == y && (element is Wall || element is Enemy || element is Player))
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
