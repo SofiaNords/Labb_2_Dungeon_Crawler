c5148f18b8e8387ea74a2aedc667645bb3007305
SofiaNords <sofia.k.nordstrom.com>
2024-10-01 08:21:34 +0200

    Add HP, AttackDice & DefenceDice props to Player

--- Labb_2_Dungeon_Crawler/Player.cs
+++ Labb_2_Dungeon_Crawler/Player.cs
@@ -1,10 +1,21 @@
 ﻿public class Player : LevelElement
 {
-    public Player(int x, int y)
+    public virtual int HP { get; set; }
+
+    public Dice AttackDice { get; set; }
+
+    public Dice DefenceDice { get; set; }
+
+    public Player(int x, int y, Dice attackDice, Dice defenceDice)
     {
         ClassChar = '@';
         Color = ConsoleColor.Magenta;
         PositionX = x;
         PositionY = y;
+        HP = 100;
+
+        AttackDice = attackDice;
+        DefenceDice = defenceDice;
     }
+
 }
