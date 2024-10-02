b586a9668d62777b112b34aa6e47ba01be408940
SofiaNords <sofia.k.nordstrom.com>
2024-10-02 15:52:00 +0200

    Remove AttackEnemy() from Player

--- Labb_2_Dungeon_Crawler/Player.cs
+++ Labb_2_Dungeon_Crawler/Player.cs
@@ -1,29 +1,28 @@
 ﻿public class Player : LevelElement
 {
-    public virtual int HP { get; set; }
+    public int HP { get; set; }
 
     public Dice AttackDice { get; set; }
 
     public Dice DefenceDice { get; set; }
 
-    public Player(int x, int y, Dice attackDice, Dice defenceDice)
+    public Player(int x, int y)
     {
         ClassChar = '@';
         Color = ConsoleColor.Magenta;
         PositionX = x;
         PositionY = y;
         HP = 100;
-
-        AttackDice = attackDice;
-        DefenceDice = defenceDice;
+        AttackDice = new Dice(2, 6, 2);
+        DefenceDice = new Dice(2, 6, 0); 
     }
 
-    public int AttackEnemy()
-    {
-        int attackScore = AttackDice.Throw();
+    //public int AttackEnemy()
+    //{
+    //    int attackScore = AttackDice.Throw();
 
-        return attackScore;
-    }
+    //    return attackScore;
+    //}
 
 
 }
