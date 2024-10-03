e19fc73c056fb786dcf13273b73625471592653a
SofiaNords <sofia.k.nordstrom.com>
2024-10-03 15:33:27 +0200

    Create Attack class and move IsAttackMove()

---
+++ Labb_2_Dungeon_Crawler/Attack.cs
@@ -0,0 +1,21 @@
+﻿public class Attack
+{
+    private LevelData _levelData;
+
+    public Attack(LevelData levelData)
+    {
+        _levelData = levelData;
+    }
+
+    public bool IsAttackMove(int x, int y)
+    {
+        foreach (var element in _levelData.Elements)
+        {
+            if (element.PositionX == x && element.PositionY == y && (element is Enemy))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
