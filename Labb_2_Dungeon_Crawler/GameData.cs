1bd890d3569a4d5f17456352b051cefeb96693cc
SofiaNords <sofia.k.nordstrom.com>
2024-10-04 07:53:40 +0200

    Create GameData class

---
+++ Labb_2_Dungeon_Crawler/GameData.cs
@@ -0,0 +1,17 @@
+﻿public class GameData
+{
+    private LevelData _levelData;
+    private Player _player;
+
+    public GameData(LevelData levelData)
+    {
+        _levelData = levelData;
+        _player = _levelData.StartPlayer;
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Name: {_player.Name} - HP: {_player.HP}");
+    }
+
+}
